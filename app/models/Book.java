383dce46bd2a035c37a61382e222df1df142e2c4
Mauritz <mauritz.sjodin3@gmail.com>
2021-07-02 09:51:21 +0200

    Much improved layout, added cover image and price as attributes for every book

--- app/models/Book.java
+++ app/models/Book.java
@@ -6,10 +6,14 @@ public class Book {
 
 	String title;
 	String author;
+	String coverImg;
+	int price;
 
-	Book(String title, String author) {
+	Book(String title, String author, String coverImg, int price) {
 		this.title = title;
 		this.author = author;
+		this.coverImg = coverImg;
+		this.price = price;
 	}
 
 	public String getTitle() {
@@ -20,15 +24,24 @@ public class Book {
 		return author;
 	}
 
+	public String getCoverImg() {
+		return coverImg;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public static ArrayList<Book> getBooks() {
 
 		String[] bookTitles = {"How to program in java play framework", "How to learn java", "Why linux is better than windows"};
 		String[] bookAuthors = {"Mauritz", "Mauritz", "Martin"};
+		int[] bookPrices = {99, 78, 149};
 
 		ArrayList<Book> books = new ArrayList<Book>(); 
 
 		for (int i=0; i<bookTitles.length; i++) {
-			books.add(new Book(bookTitles[i], bookAuthors[i]));
+			books.add(new Book(bookTitles[i], bookAuthors[i], "assets/images/book-covers-big-2019101610.jpg", bookPrices[i]));
 		}
 
 		return books;
