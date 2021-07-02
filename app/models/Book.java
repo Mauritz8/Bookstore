916659dab8e352cc456ad5004385f12b4914248b
Mauritz <mauritz.sjodin3@gmail.com>
2021-07-02 17:54:42 +0200

    changed book cover, and fixed so the book covers are not a fixed size

--- app/models/Book.java
+++ app/models/Book.java
@@ -8,9 +8,9 @@ public class Book {
 	String title;
 	String author;
 	String coverImg;
-	int price;
+	double price;
 
-	Book(int id, String title, String author, String coverImg, int price) {
+	Book(int id, String title, String author, String coverImg, double price) {
 		this.id = id;
 		this.title = title;
 		this.author = author;
@@ -34,7 +34,7 @@ public class Book {
 		return coverImg;
 	}
 
-	public int getPrice() {
+	public double getPrice() {
 		return price;
 	}
 
@@ -42,12 +42,12 @@ public class Book {
 
 		String[] bookTitles = {"How to program in java play framework", "How to learn java", "Why linux is better than windows", "Why I love java", "Should you learn C#?", "How to program in java play framework", "How to learn java", "Why linux is better than windows", "Why I love java", "Should you learn C#?"};
 		String[] bookAuthors = {"Mauritz", "Mauritz", "Martin", "Everyone", "Tobias", "Mauritz", "Mauritz", "Martin", "Everyone", "Tobias"};
-		int[] bookPrices = {99, 78, 149, 199, 129, 99, 78, 149, 199, 129};
+		double[] bookPrices = {9.9, 7.8, 14.9, 19.9, 12.9, 9.9, 7.8, 14.9, 19.9, 12.9};
 
 		ArrayList<Book> books = new ArrayList<Book>(); 
 
 		for (int i=0; i<bookTitles.length; i++) {
-			books.add(new Book(i, bookTitles[i], bookAuthors[i], "/assets/images/book-covers-big-2019101610.jpg", bookPrices[i]));
+			books.add(new Book(i, bookTitles[i], bookAuthors[i], "/assets/images/book-cover.jpg", bookPrices[i]));
 		}
 
 		return books;
